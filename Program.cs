c854ed0897689d40da01a3974ecdb441bb6c4873
juan carlos osorno <juan.osorno341@pascualbravo.edu.co>
2024-02-20 13:59:28 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clas3Poo20defeb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es la marca del vehiculo");
+            string marca = Console.ReadLine();
+            Console.WriteLine("cual es el modelo del vehiculo");
+            string modelo = Console.ReadLine();
+            Console.WriteLine("Cual es el cilindraje del vehiculo");
+            string cilindraje = Console.ReadLine();
+
+            Console.WriteLine("Cual es el precio del vehiculo");
+            double precio = double.Parse(Console.ReadLine());
+
+
+            Vehiculo vehiculo = new Vehiculo(marca, modelo, cilindraje, precio);
+            Vehiculo mivehiculo = new  Vehiculo();
+
+
+            Console.WriteLine("\n\n\t\tMarca: " + vehiculo.Marca);
+            Console.WriteLine("\t\tModelo: " + vehiculo.Modelo);
+            Console.WriteLine("\t\tCilindraje: " + vehiculo.Cilindraje);
+            Console.WriteLine("\t\tPrecio: " + vehiculo.Precio);
+            mivehiculo.Mostrar();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Vehiculo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clas3Poo20defeb
+{
+    internal class Vehiculo
+    {
+        string marca;
+        string modelo;
+        string cilindraje;
+        double precio;
+
+        public Vehiculo(string marca, string modelo, string cilindraje, double precio)
+        {
+            this.Marca = marca;
+            this.Modelo = modelo;
+            this.Cilindraje = cilindraje;
+            this.Precio = precio;
+        }
+
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public string Cilindraje { get => cilindraje; set => cilindraje = value; }
+        public double Precio { get => precio; set => precio = value; }
+
+        public Vehiculo() 
+        {
+
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("Estoy en el metodo mostrar de la clase vehículo");
+        }
+    }
+}
